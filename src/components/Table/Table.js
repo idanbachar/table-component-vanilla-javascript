abe34a47705037f0cf6ddeec060ef4e5db97c2fb
Idan Bachar <55582129+idanbachar@users.noreply.github.com>
2021-06-29 15:48:07 +0300

    created pagination component to handle pagination

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import useSortableData from '../../useSortableData';
+import Pagination from './Pagination';
 
 export default function Table() {
     const users = useSelector(state => state.users);
     const [text, setText] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
 
     const { items, requestSort } = useSortableData(users);
 
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
     const TableHeader = () => {
-        return (Object.keys(items[0]).map(attr =>
+        return (Object.keys(currentItems[0]).map(attr =>
             <th key={attr}>
                 <button onClick={() => requestSort(attr)}>
                     {attr.toUpperCase()}
@@ -19,7 +28,7 @@ export default function Table() {
     }
 
     const TableRows = () => {
-        return items.filter((item) => {
+        return currentItems.filter((item) => {
             const stringifyRow = JSON.stringify(item);
             if (text === "") return item;
             if (stringifyRow.toLowerCase().includes(text.toLowerCase()))
@@ -47,17 +56,20 @@ export default function Table() {
                 <option>api filtering</option>
             </select>
 
-            {items.length > 0 ?
-                <table>
-                    <thead>
-                        <tr>
-                            <TableHeader />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <TableRows />
-                    </tbody>
-                </table>
+            {currentItems.length > 0 ?
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <TableHeader />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <TableRows />
+                        </tbody>
+                    </table>
+                    <Pagination itemsPerPage={itemsPerPage} totalItems={items.length} paginate={paginate} />
+                </div>
                 : <h1>
                     No data :(
                 </h1>}
